a4d17c509393fdf84e119e4e1383790bb789fde2
aro-freelance <aro.freelance@gmail.com>
2022-11-10 16:09:21 -0500

    initial commit
    Next... go through my project in android and use it to setup the structure of this project

---
+++ AroraCountdown/Controller/AddCounterViewController.swift
@@ -0,0 +1,36 @@
+//
+//  AddCounterViewController.swift
+//  AroraCountdown
+//
+//  Created by Mandy on 11/10/22.
+//
+
+import UIKit
+import RealmSwift
+
+
+//this VC is for letting the user set up and save a new counter
+
+class AddCounterViewController: UITableViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+
+}
---
+++ AroraCountdown/Model/Countdown.swift
@@ -0,0 +1,18 @@
+//
+//  Countdown.swift
+//  AroraCountdown
+//
+//  Created by Mandy on 11/10/22.
+//
+
+import Foundation
+
+//this file defines what each countdown object will have
+
+class Countdown {
+    
+    
+    
+    
+    
+}
